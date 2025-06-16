02824277f82da57515dfd64a1b1e81f8ee1df884
Harun Al Rasyid <harundarat@gmail.com>
2025-06-16 21:42:39 +0700

    struct

---
+++ struct.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type Customer struct {
+	Name, Address string
+	Age           int
+}
+
+func (customer Customer) sayHello(name string) {
+	fmt.Println("Hello ", name, " my name is ", customer.Name)
+}
+
+func main() {
+	var harun Customer
+
+	harun.Name = "Harun"
+	harun.Address = "Indonesia"
+	harun.Age = 22
+
+	fmt.Println(harun.Name)
+	fmt.Println(harun.Age)
+	fmt.Println(harun)
+
+	al := Customer{
+		Name:    "Al",
+		Address: "Indonesia",
+		Age:     22,
+	}
+	fmt.Println(al)
+
+	rasyid := Customer{
+		Name:    "Rasyid",
+		Address: "Indonesia",
+		Age:     22,
+	}
+	fmt.Println(rasyid)
+
+	harun.sayHello("Vincent")
+	al.sayHello("Vincent")
+	rasyid.sayHello("Vincent")
+}
